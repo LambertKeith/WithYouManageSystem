4a408cec4b3a197d519217ffa8568adcfc315ebd
lambert keith <2698730244@qq.com>
2024-01-02 00:30:15 +0800

    1

--- with_you_system/login/models.py
+++ with_you_system/login/models.py
@@ -5,20 +5,20 @@ from django.core.validators import MinLengthValidator
 
 class Teacher(models.Model):
     # 教师编号，假设是唯一的
-    teacher_id = models.CharField(max_length=10, unique=True)
+    teacher_id = models.CharField(verbose_name='教师编号', max_length=10, unique=True)
     
     # 姓名
-    name = models.CharField(max_length=100)
+    name = models.CharField(verbose_name='教师姓名', max_length=100)
 
     # 负责教室
-    classroom = models.CharField(max_length=50)
+    classroom = models.CharField(verbose_name='负责教室', max_length=50)
 
     # 登录密码
     # 注意：实际中应使用Django内置的用户认证系统来处理密码
-    password = models.CharField(max_length=50, validators=[MinLengthValidator(8)])
+    password = models.CharField(verbose_name='登录密码', max_length=50, validators=[MinLengthValidator(8)])
 
     # 是否为管理员，布尔值，默认为False
-    is_admin = models.BooleanField(default=False)
+    is_admin = models.BooleanField(verbose_name='是否为管理员', default=False)
 
     def __str__(self):
         return self.name
